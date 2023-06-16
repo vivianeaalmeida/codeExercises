0c81a01812415e63568c68ade58d5ca3599ac86d
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-06-16 10:32:09 +0100

    Create abbrevName.js

---
+++ abbrevName.js
@@ -0,0 +1,12 @@
+function abbrevName(name){
+    return name.split(" ").map((n)=>n[0]).join(".");
+}
+
+
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+
+*/
