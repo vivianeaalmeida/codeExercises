1197a0f872604b7bd3edb6691337bcdda2f28c84
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-06-29 15:15:38 +0100

    Update inputArray.js

--- inputArray.js
+++ inputArray.js
@@ -8,3 +8,13 @@ function solution(inputArray) {
 
 //nao passa em todos os testes
 //length - 1 para ele parar um elemento antes e sempre ter um seguinte.
+
+/* SOLUÇÃO CORRETA: pois queria que eu multiplicasse cada elemento lado a lado, e não em pares. Ops Viviane
+function solution(inputArray) {
+    let product = [];
+    for(let i = 0; i < inputArray.length - 1; i++){
+        product.push(inputArray[i]*inputArray[i + 1])
+    }
+    return Math.max(...product);
+}
+*/
