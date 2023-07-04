56ebdc6dce104c74a8d12d84b7b14d689c70d37f
Viviane Andrade de Almeida <81475990+vivianeaalmeida@users.noreply.github.com>
2023-07-04 14:22:20 +0100

    Create sumMix.js

---
+++ sumMix.js
@@ -0,0 +1,26 @@
+function sumMix(x){
+let sum = 0;
+let newArray = [];
+let inteiro = 0;
+  
+    for(let i = 0; i < x.length; i++){
+      inteiro = parseInt(x[i]);
+      newArray.push(inteiro);
+    }
+
+    for(let i = 0; i < newArray.length; i++){
+      sum += newArray[i];
+    }
+
+    return sum;
+  }
+
+/*
+function sumMix(x){
+  let result = 0;
+  for (let n of x) {
+    result += parseInt(n);
+  }
+  return result;
+}
+*/
